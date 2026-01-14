a14b45124182f115545cba361e42b245d558dfc3
indecisive_943528 <tom@allen.wales>
2026-01-14 19:52:05 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,14 @@
 local HttpService = game:GetService("HttpService")
 
 local WEBHOOK = "https://discord.com/api/webhooks/1461077762130444479/T9wBoye7K5t6Tj-ay4f7j6KN0NAV7CUEHsYf4QbKDG62HZyfnYGyvk-7_oZ6IbFFpxHK"
+
+local printstatus = 0
+local function printstate()
+    print(printstatus)
+    printstatus = printstatus + 1
+end
 -- a
+printstate()
 local logins = {
     { user = "test1", password = "pass2", hwid = "" },
     { user = "test",  password = "pass",  hwid = "balls" }
@@ -39,17 +46,17 @@ local function getHwid()
         return tostring(hash)
     end
 end
-
+printstate()
 local user = getgenv().user
 local password = getgenv().password
 
 if not user or not password then
     hardStop()
 end
-
+printstate()
 local hwid = getHwid()
 local found
-
+printstate()
 for _, v in ipairs(logins) do
     if v.user == user and v.password == password then
         found = v
@@ -60,9 +67,9 @@ end
 if not found then
     hardStop()
 end
-
+printstate()
 found.hwid = found.hwid or ""
-
+printstate()
 if found.hwid == "" then
     task.spawn(function()
         request({
@@ -73,15 +80,16 @@ if found.hwid == "" then
                 content =
                     "new hwid bind\n" ..
                     "user: " .. found.user .. "\n" ..
-                    "hwid: " .. hwid
+                    "hwid: ``` " .. hwid .. " ```"
             })
         })
     end)
 
 elseif found.hwid ~= hwid then
+    print("hwid invalid, open a ticket")
     hardStop()
 end
-
+printstate()
 loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/petal-cymk/repotestaaaa/refs/heads/main/something/something/something/something/something/main.lua"
 ))()
