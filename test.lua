eab88e4e0d1ebd220a303f58791286d4cbf6055f
indecisive_943528 <tom@allen.wales>
2026-01-16 17:43:34 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,17 +1,19 @@
 local HttpService = game:GetService("HttpService")
+local AnalyticsService = game:GetService("RbxAnalyticsService")
 
 local WEBHOOK = "https://discord.com/api/webhooks/1461397355738828903/D5SuLOc5GKzEdvU23qn5GM0wilHwxh8i45ngp_BdHzosvFvL5WHDF2xvEcfT659vp5gQ"
 
 local printstatus = 0
 local function printstate()
     print(printstatus)
-    printstatus = printstatus + 1
+    printstatus += 1
 end
--- abcd
+
 printstate()
+
 local logins = {
-    { user = "fusion", password = "admin", hwid = "907414537" },
-    { user = "silke",  password = "Imafemboy",  hwid = "" },
+    { user = "fusion", password = "admin", hwid = "" },
+    { user = "silke",  password = "Imafemboy", hwid = "" },
 }
 
 local function hardStop()
@@ -19,44 +21,39 @@ local function hardStop()
 end
 
 local function getHwid()
-    local res = request({
-        Url = "https://httpbin.org/headers",
-        Method = "GET"
-    })
-
-    if not res or not res.Body then
+    local cid = AnalyticsService:GetClientId()
+    if not cid then
         return "hwid_fail"
     end
 
-    local data = HttpService:JSONDecode(res.Body)
-    local h = data.headers or {}
-
-    local raw =
-        (h["User-Agent"] or "") ..
-        (h["Accept-Language"] or "") ..
-        (h["Accept-Encoding"] or "")
-
     if syn and syn.crypt and syn.crypt.hash then
-        return syn.crypt.hash(raw, "sha256")
-    else
-        local hash = 0
-        for i = 1, #raw do
-            hash = (hash * 31 + raw:byte(i)) % 4294967296
-        end
-        return tostring(hash)
+        return syn.crypt.hash(cid, "sha256")
     end
+
+    local hash = 0
+    for i = 1, #cid do
+        hash = (hash * 31 + cid:byte(i)) % 4294967296
+    end
+
+    return tostring(hash)
 end
+
 printstate()
+
 local user = getgenv().user
 local password = getgenv().password
 
 if not user or not password then
     hardStop()
 end
+
 printstate()
+
 local hwid = getHwid()
 local found
+
 printstate()
+
 for _, v in ipairs(logins) do
     if v.user == user and v.password == password then
         found = v
@@ -67,9 +64,13 @@ end
 if not found then
     hardStop()
 end
+
 printstate()
+
 found.hwid = found.hwid or ""
+
 printstate()
+
 if found.hwid == "" then
     task.spawn(function()
         request({
@@ -80,16 +81,17 @@ if found.hwid == "" then
                 content =
                     "new hwid bind\n" ..
                     "user: " .. found.user .. "\n" ..
-                    "hwid: ```" .. hwid .. "```" 
+                    "hwid: ```" .. hwid .. "```"
             })
         })
     end)
-
 elseif found.hwid ~= hwid then
     print("hwid invalid, open a ticket")
     hardStop()
 end
+
 printstate()
+
 loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/petal-cymk/repotestaaaa/refs/heads/main/something/something/something/something/something/main.lua"
 ))()
