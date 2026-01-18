387eaab24b240d5b3e8803e477c58a72d0067b3d
indecisive_943528 <tom@allen.wales>
2026-01-18 00:11:57 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -8,23 +8,26 @@ local WEBHOOK = "https://discord.com/api/webhooks/1461397355738828903/D5SuLOc5GK
 
 local logins = {
     { user = "fusion", password = "admin", hwid = "907414537" },
-    { user = "silke",  password = "Imafemboy",  hwid = "" },
+    { user = "silke",  password = "Imafemboy",  hwid = "0" },
 }
 
 local function getHwid()
     local res = request({ Url = "https://httpbin.org/headers", Method = "GET" })
     if not res or not res.Body then return "hwid_fail" end
-    local data = HttpService:JSONDecode(res.Body)
-    local h = data.headers or {}
-    local raw = (h["User-Agent"] or "") .. (h["Accept-Language"] or "") .. (h["Accept-Encoding"] or "")
-    if syn and syn.crypt and syn.crypt.hash then
-        return syn.crypt.hash(raw,"sha256")
-    else
+    local body = res.Body
+
+    local function extract(key)
+        local pattern = '"'..key..'":%s*"([^"]+)"'
+        return body:match(pattern) or ""
+    end
+
+    local raw = extract("User-Agent") .. extract("Accept-Language") .. extract("Accept-Encoding")
         local hash = 0
         for i=1,#raw do hash = (hash*31 + raw:byte(i))%4294967296 end
         return tostring(hash)
     end
 end
+
 
 task.spawn(function()
     while true do
